6f8d094f3f59bef68e9a5f40f18e43c30811ab82
William Carson <williamacarson1@outlook.com>
2025-08-26 21:31:29 -0700

    simple transition

--- src/lib.rs
+++ src/lib.rs
@@ -1,136 +1,51 @@
-use heck::ToSnakeCase;
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Ident, Token};
-use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, DeriveInput};
 
-// Input parser for the macro
-struct StateMachineInput {
-    struct_name: Ident,
-    states: Vec<Ident>,
-}
-
-impl Parse for StateMachineInput {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        let struct_name: Ident = input.parse()?;
-        input.parse::<Token![;]>()?;
-        let states = syn::punctuated::Punctuated::<Ident, Token![,]>::parse_terminated(input)?
-            .into_iter()
-            .collect();
-        Ok(StateMachineInput {
-            struct_name,
-            states,
-        })
-    }
-}
-
-#[proc_macro]
-pub fn state_machine(input: TokenStream) -> TokenStream {
-    let StateMachineInput { struct_name, states } = parse_macro_input!(input as StateMachineInput);
+/// Derive macro for simple events that don't need phase-specific payloads.
+/// 
+/// This macro implements `TransitionEvent` for simple events by setting all
+/// associated types to `NoEvent` and returning `None` for all phase methods.
+/// 
+/// # Example
+/// 
+/// ```rust
+/// use bevy::prelude::*;
+/// use bevy_gearbox_macros::SimpleTransition;
+/// 
+/// #[derive(Event, Clone, SimpleTransition)]
+/// struct MySimpleEvent;
+/// ```
+/// 
+/// This is equivalent to manually implementing:
+/// 
+/// ```rust
+/// impl TransitionEvent for MySimpleEvent {
+///     type ExitEvent = NoEvent;
+///     type EffectEvent = NoEvent;
+///     type EntryEvent = NoEvent;
+///     
+///     fn to_exit_event(&self) -> Option<Self::ExitEvent> { None }
+///     fn to_effect_event(&self) -> Option<Self::EffectEvent> { None }
+///     fn to_entry_event(&self) -> Option<Self::EntryEvent> { None }
+/// }
+/// ```
+#[proc_macro_derive(SimpleTransition)]
+pub fn derive_simple_transition(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
     
-    let first_state_ident = states.get(0).expect("State machine must have at least one state.");
-    
-    // Generate state enum
-    let enum_name = Ident::new(
-        &format!("{}StateEnum", struct_name.to_string()),
-        struct_name.span(),
-    );
-    let enum_variants = states.iter().enumerate().map(|(i, current_state)| {
-        if i == 0 {
-            quote! {
-                #[default]
-                #current_state,
-            }
-        } else {
-            quote! {
-                #current_state,
-            }
-        }
-    });
-
-    let enum_system_name = Ident::new(
-        &format!("{}_enum_trigger_system", struct_name.to_string().to_snake_case()),
-        struct_name.span(),
-    );
-
-    // Generate trigger systems for each state
-    let trigger_systems = states.iter().map(|current_state| {
-        let system_name = Ident::new(
-            &format!("{}_{}_trigger_system", struct_name.to_string().to_snake_case(), current_state.to_string().to_snake_case()),
-            current_state.span(),
-        );
-        let remove_other_states = states.iter().filter(|&state| state != current_state).map(|state| {
-            quote! {
-                #enum_name::#state => {
-                    bevy_gearbox::commands::StateExitCommandsExt::try_exit_state::<#state>(&mut c, state.clone());
-                }
-            }
-        });
-
-        quote! {
-            fn #system_name(
-                trigger: Trigger<bevy_gearbox::commands::Transition<#current_state>>,
-                mut query: Query<&mut #enum_name, With<#struct_name>>,
-                mut commands: Commands,
-            ) {
-                let Ok(mut state_machine_enum) = query.get_mut(trigger.target()) else {
-                    return;
-                };
-                let mut c = commands.entity(trigger.target());
-                let state = &trigger.0;
-                match *state_machine_enum {
-                    #(#remove_other_states)*,
-                    #enum_name::#current_state => (),
-                }
-                *state_machine_enum = #enum_name::#current_state;
-            }
-        }
-    });
-
-    // Add systems to the plugin
-    let add_systems = states.iter().map(|state| {
-        let system_name = Ident::new(
-            &format!("{}_{}_trigger_system", struct_name.to_string().to_snake_case(), state.to_string().to_snake_case()),
-            state.span(),
-        );
-        quote! {
-            .add_observer(#system_name)
-        }
-    });
-
-    // Generate the plugin name
-    let plugin_name = Ident::new(&format!("{}Plugin", struct_name), struct_name.span());
-
-    // Generate the expanded code
     let expanded = quote! {
-        #[derive(Component, Clone, Debug, Default, Reflect)]
-        enum #enum_name {
-            #(#enum_variants)*
-        }
-
-        fn #enum_system_name(
-            trigger: Trigger<OnAdd, #struct_name>,
-            mut commands: Commands,
-        ) {
-            let entity = trigger.target();
-        
-            commands.entity(entity).insert(#enum_name::default());
-            commands.entity(entity).insert(#first_state_ident::default());
-        }
-
-        #(#trigger_systems)*
-
-        pub struct #plugin_name;
-
-        impl Plugin for #plugin_name {
-            fn build(&self, app: &mut App) {
-                app
-                    #(#add_systems)*
-                    .add_observer(#enum_system_name)
-                    .register_type::<#enum_name>();
-            }
+        impl bevy_gearbox::TransitionEvent for #name {
+            type ExitEvent = bevy_gearbox::NoEvent;
+            type EffectEvent = bevy_gearbox::NoEvent;
+            type EntryEvent = bevy_gearbox::NoEvent;
+            
+            fn to_exit_event(&self) -> Option<Self::ExitEvent> { None }
+            fn to_effect_event(&self) -> Option<Self::EffectEvent> { None }
+            fn to_entry_event(&self) -> Option<Self::EntryEvent> { None }
         }
     };
-
+    
     TokenStream::from(expanded)
 }
